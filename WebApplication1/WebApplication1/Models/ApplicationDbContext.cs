a34d1d622ab934ce9942154e6e5894727a6857a4
Bacelek <pasha.matl@mail.ru>
2025-10-20 10:07:51 +0300

    DB connection

---
+++ WebApplication1/WebApplication1/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+        
+    }
+}
